7a43351f2e78aa407c2d071eb9cf218583a1b823
your-name <184022686+stupid-nerd-01111000@users.noreply.github.com>
2024-12-25 19:08:32 +0330

    update login window

--- app.py
+++ app.py
@@ -19,11 +19,31 @@ class Login_window(QWidget):
         self.title_label = QLabel('login to your account')
         self.title_label.setFont(QFont('Arial', 16))
         self.title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        #username_field
+        self.username_input = QLineEdit()
+        self.username_input.setPlaceholderText('Enter your username')
+
+        #password_field
+        self.password_input = QLineEdit()
+        self.password_input.setPlaceholderText('Enter your password')
+
+        #login_button
+        self.login_button = QPushButton('Login')
+        self.login_button.clicked.connect(self.send_data)
+
+
 
         #add_widgets_to_layout
         layout.addWidget(self.title_label)
+        layout.addWidget(self.username_input)
+        layout.addWidget(self.password_input)
+        layout.addWidget(self.login_button)
 
         self.setLayout(layout)
+    
+    def send_data(self):
+        pass
 
 
 def receive_data():
