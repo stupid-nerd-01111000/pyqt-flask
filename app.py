e05227956208afee5269d7ce0b43e1a5a0aca9f5
your-name <184022686+stupid-nerd-01111000@users.noreply.github.com>
2024-12-25 18:34:13 +0330

    add title label

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 import requests
 import sys
-from PyQt6.QtWidgets import *
+from PyQt6.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox, QWidget
+from PyQt6.QtGui import QFont
+from PyQt6.QtCore import Qt
+
+
+class Login_window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Login page')
+        self.setGeometry(300, 300, 650, 650)
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QVBoxLayout(self)
+
+        #login_title
+        self.title_label = QLabel('login to your account')
+        self.title_label.setFont(QFont('Arial', 16))
+        self.title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        #add_widgets_to_layout
+        layout.addWidget(self.title_label)
+
+        self.setLayout(layout)
 
 
 def receive_data():
@@ -8,3 +31,10 @@ def receive_data():
 
 def send_data():
     pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Login_window()
+    window.show()
+    sys.exit(app.exec())
